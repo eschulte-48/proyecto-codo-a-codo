f3910e69a3f1171ccd462ab5ba5fd464ab138e5f
ariana <arianacampos199@gmail.com>
2022-10-23 16:34:33 -0300

    header dinamico

--- js/header.js
+++ js/header.js
@@ -1,4 +1,5 @@
-$(document).ready(function(){
+/* $(document).ready(function(){
+    
     $(window).scroll(function(){
         if( $(this).scrollTop() < 0){
             $('header').addClass('header2');
@@ -7,6 +8,10 @@ $(document).ready(function(){
         }
     });
 
-});
+}); */
 
 
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("abajo", window.scrollY>0);
+})
